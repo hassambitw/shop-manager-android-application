948f269a92c5ebd8e53d20c8716a48ab651b669c
Loner291999 <hassan291999@gmail.com>
2020-11-13 11:46:56 +0400

    trying to check if my commif my commit works, istg this whole thing is really laggy

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //trying to put a comment to see if this wiorks - hassan
     }
 }
