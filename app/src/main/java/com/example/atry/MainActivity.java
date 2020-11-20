ffb3b25388cc02b3ce1ac13fe1410e03904cba05
Loner291999 <hassan291999@gmail.com>
2020-11-20 23:57:45 +0400

    added navigation bar, need to add fragments

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.atry;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    public DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+
+        drawer = findViewById(R.id.drawer_layout); //menu
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         //trying to put a comment to see if this wiorks - hassan
         //adding another comment to show hassam how to commit
         //test again
     }
+
+    //when you want to go back, you dont want to close activity only close nav bar
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.END);
+        }else{
+            super.onBackPressed();
+        }}
 }
