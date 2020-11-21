6f4dac995eb7629a4164c45af07e38236c0f6fd7
Loner291999 <hassan291999@gmail.com>
2020-11-21 11:06:30 +0400

    -added 2 fragment xmls and their fragment classes, along with the basic framework on how to call them. - readjusted the header and the main activity class a bit - added a new "appname" which definitely sounded better than "Try" - added some explaination to how some of it works, i hope you guys might understand it tho i will link a video or 2 later in the chat which would explain the stuff i did and the stuff we mare expected to work

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -9,10 +9,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public DrawerLayout drawer;
     @Override
@@ -22,19 +24,33 @@ public class MainActivity extends AppCompatActivity {
 
 
         drawer = findViewById(R.id.drawer_layout); //menu
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar); //makes your own toolbar, needed inorder to make your nav drawer functional
+        //also remove the other actionbar using NoActionBar theme
         setSupportActionBar(toolbar);
 
+        NavigationView nav_view = findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this); //for now i am implemmenting thelistner to this class but in future(or asap)
+        // we can try changing it to its own class or something once we have a better grasp of this
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle); //this is for the toggle, it allows the toggle animation
         toggle.syncState();
+
+
         //trying to put a comment to see if this wiorks - hassan
         //adding another comment to show hassam how to commit
         //test again
+        // when theres nothing in the saved state ie when the activity restarts or resumes, it will get the deashboard fragment
+        if (savedInstanceState ==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new DashboardFragment()).commit();
+            nav_view.setCheckedItem(R.id.nav_dashb1);
+        }
+
+        //note to self, all fragment classes must be a public class inorder for it to function
     }
 
-    //when you want to go back, you dont want to close activity only close nav bar
+    //when you want to go back, you dont want to close activity only close nav bar when navbar open
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)){
@@ -42,4 +58,48 @@ public class MainActivity extends AppCompatActivity {
         }else{
             super.onBackPressed();
         }}
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_dashb1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new DashboardFragment()).commit();
+                break;
+            case R.id.nav_dashb2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new DashboardFragment()).commit();
+                break;
+            case R.id.nav_product:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProductFragment()).commit();
+                break;
+            case R.id.nav_other:
+                Toast.makeText(this,"Other",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_purchase:
+                Toast.makeText(this,"Purchases",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_report:
+                Toast.makeText(this,"Reports",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_sale:
+                Toast.makeText(this,"Sale",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_setting:
+                Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.share:
+                Toast.makeText(this,"Test ?",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.send:
+                Toast.makeText(this,"Settings ?",Toast.LENGTH_SHORT).show();
+                break;
+
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
